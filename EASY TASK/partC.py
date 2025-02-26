c418929a61a7084880301d2c75aab77525764834
ElizabethMM36 <194572580+ElizabethMM36@users.noreply.github.com>
2025-02-26 12:24:01 +0530

    commited

--- EASY TASK/partC.py
+++ EASY TASK/partC.py
@@ -15,10 +15,12 @@ win.fill((255,255, 255)) # for a white background
 font = pygame.font.SysFont('arial',24)
 draw=True
 tdistance = 0 # to calculate the total disrance
-run=True
+run=True # runs the main loop
 radius=10
 pos=(0,0)
-points=[] 
+points=[] # stores the points of the created circles
+
+# To calculate the distance between the points
 def calculatedist(p1,p2):
     return math.sqrt((p2[0] -p1[0])**2 + (p2[1] -p1[1])**2)
 
@@ -37,7 +39,7 @@ def draw():
         path_text = f"Total Path Length: {tdistance:.2f} pixels"
         text = font.render(path_text,True,(0,0,0))
         win.blit(text, (10, 10))
-    # Display the total distance
+    # Update the display after each draiwng
      pygame.display.update()
     
        
@@ -54,27 +56,27 @@ while run:
             if event.button == 1:
                 mouse_pos=event.pos
                 points.append(mouse_pos)
-                
+                 # calculate the total points
 
                 if len(points) >1:
                     distance = calculatedist(points[-2],points[-1])
                     tdistance += distance
                     print(f"Added point {len(points)}.Distance from previous point : {distance : 2f}")
 
-            elif event.button == 3 and points :
+            elif event.button == 3 and points : # right click to delete the last circle
                 if len(points) > 1:
                     tdistance -= calculatedist(points[-2],points[-1])
                 points.pop()
             
-        elif event.type == pygame.KEYDOWN :
+        elif event.type == pygame.KEYDOWN : # click spacebar to clear the creen
             if event.key == pygame.K_SPACE:
                 points =[]
                 tdistance = 0
 
-            elif event.key == pygame.K_s :
+            elif event.key == pygame.K_s : # click the s button to save
                 pygame.image.save(win,"drawing.png")
             
-    draw()
+    draw() # redraw and update the creen
 
         
     
